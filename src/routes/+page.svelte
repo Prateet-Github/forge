45e3b982196d2924acf3b86427bfbe5cbb1e9a39
Prateet Tiwari <prateettiwari29@gmail.com>
2025-12-13 04:16:24 +0530

    feat: integrate Tailwind CSS and update styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div
+  class="min-h-screen flex justify-center items-center mx-auto bg-white dark:bg-black text-black dark:text-white"
+>
+  <h1 class="text-3xl font-bold underline">Hello Svelte!</h1>
+</div>
