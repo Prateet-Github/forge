edd6e53565f16ef8ac146cc01c356c778cde8443
Prateet Tiwari <prateettiwari29@gmail.com>
2025-12-13 18:03:55 +0530

    feat: add login and signup forms

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,34 @@
+<form
+  method="POST"
+  class="min-h-screen w-full bg-white dark:bg-black text-black dark:text-white flex justify-center items-center mx-auto px-4"
+>
+  <div
+    class="flex flex-col gap-4 justify-center items-center border border-gray-300 p-4 dark:border-gray-800 rounded-2xl max-w-sm w-full"
+  >
+    <h1 class="text-2xl font-bold">Login Page</h1>
+    <input
+      name="email"
+      type="text"
+      placeholder="Email"
+      class="border border-gray-300 p-4 dark:border-gray-800 rounded-2xl outline-none w-full"
+    />
+    <input
+      name="password"
+      type="password"
+      placeholder="Password"
+      class="border border-gray-300 p-4 dark:border-gray-800 rounded-2xl outline-none w-full"
+    />
+    <p class="text-xs font-semibold">
+      Don't have an Account?{""}
+      <span class="hover:underline cursor-pointer">
+        <a href="/signup">Sign Up</a>
+      </span>
+    </p>
+    <button
+      type="submit"
+      class="p-4 bg-black dark:bg-white text-white dark:text-black rounded-2xl w-full cursor-pointer hover:opacity-80 transition"
+    >
+      Login
+    </button>
+  </div>
+</form>
---
+++ src/routes/signup/+page.svelte
@@ -0,0 +1,48 @@
+<form
+  method="POST"
+  class="min-h-screen w-full bg-white dark:bg-black text-black dark:text-white flex justify-center items-center mx-auto px-4"
+>
+  <div
+    class="flex flex-col gap-4 justify-center items-center border border-gray-300 p-4 dark:border-gray-800 rounded-2xl max-w-sm w-full"
+  >
+    <h1 class="text-2xl font-bold text-center">Create a new Account</h1>
+    <input
+      name="name"
+      type="text"
+      placeholder="Email"
+      class="border border-gray-300 p-4 dark:border-gray-800 rounded-2xl outline-none w-full"
+    />
+    <input
+      name="username"
+      type="text"
+      placeholder="Username"
+      class="border border-gray-300 p-4 dark:border-gray-800 rounded-2xl outline-none w-full"
+    />
+    <input
+      name="email"
+      type="email"
+      placeholder="Email"
+      class="border border-gray-300 p-4 dark:border-gray-800 rounded-2xl outline-none w-full"
+    />
+    <input
+      name="password"
+      type="password"
+      placeholder="Password"
+      class="border border-gray-300 p-4 dark:border-gray-800 rounded-2xl outline-none w-full"
+    />
+
+    <p class="text-xs font-semibold">
+      Already have an Account?{""}
+      <span class="hover:underline cursor-pointer">
+        <a href="/login">Login</a>
+      </span>
+    </p>
+
+    <button
+      type="submit"
+      class="p-4 bg-black dark:bg-white text-white dark:text-black rounded-2xl w-full cursor-pointer hover:opacity-80 transition"
+    >
+      Sign Up
+    </button>
+  </div>
+</form>
